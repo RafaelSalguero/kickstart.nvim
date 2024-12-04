d066b4ec90ecb7fdc411e5d7651eed7dec0f244d
rafaels <rafaels@zillowgroup.com>
2024-12-04 10:38:14 -0700

    tree ignore node_modules

--- init.lua
+++ init.lua
@@ -363,13 +363,12 @@ require('lazy').setup({
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
         -- pickers = {}
+        defaults = {
+          file_ignore_patterns = {
+            'node_modules',
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
