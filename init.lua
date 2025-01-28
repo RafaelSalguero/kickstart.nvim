a9e78dfc7d387bbd63588c33724b00759c6228b5
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2025-01-28 14:01:44 -0700

    git blame

--- init.lua
+++ init.lua
@@ -315,6 +315,15 @@ require("lazy").setup({
 		end,
 	},
 	{
+		"f-person/git-blame.nvim",
+		event = "VeryLazy",
+		opts = {
+			message_template = " <author> • <date>• <summary> • <<sha>>", -- template for the blame message, check the Message template section for more options
+			date_format = "%Y-%m-%d %H:%M:%S", -- template for the date, check Date format section for more options
+			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+		},
+	},
+	{
 		"esmuellert/nvim-eslint",
 		config = function()
 			require("nvim-eslint").setup({})
