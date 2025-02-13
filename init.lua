33bbf74250ee9bf7ed1969b496ffae00fcd31a5c
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2025-02-13 14:17:37 -0700

    add sticky buff

--- init.lua
+++ init.lua
@@ -275,6 +275,20 @@ require("lazy").setup({
 	{
 		"rickhowe/diffchar.vim",
 	},
+	{
+		"stevearc/stickybuf.nvim",
+		config = function()
+			require("stickybuf").setup({
+				-- This function is run on BufEnter to determine pinning should be activated
+				get_auto_pin = function(bufnr)
+					-- You can return "bufnr", "buftype", "filetype", or a custom function to set how the window will be pinned.
+					-- You can instead return an table that will be passed in as "opts" to `stickybuf.pin`.
+					-- The function below encompasses the default logic. Inspect the source to see what it does.
+					return require("stickybuf").should_auto_pin(bufnr)
+				end,
+			})
+		end,
+	},
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	{
 		"akinsho/toggleterm.nvim",
